e357b17227e24f8e42d3beaec7dc52c32adcf87a
Vibhakar <vibhakarsenth@gmail.com>
2025-04-29 18:48:40 +0530

    feat(ui): Home page Implemented

--- src/app/(screen)/page.tsx
+++ src/app/(screen)/page.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="container flex items-center justify-center flex-col gap-4 mx-auto pt-15">
+      <h1 className="lg:text-5xl text-3xl text-center font-bold leading-normal">
+        We Build the Website Your
+        <br /> Competitors Wish They
+        <br /> Had.
+      </h1>
+      <div className="flex items-center justify-center flex-row gap-4 pt-5">
+        <Button color="gray" variant="solid" className="bg-white text-black">
+          <Link href={"/work"}>Our Projects</Link>
+        </Button>
+        <Button variant={"outline"} asChild>
+          <Link href={"/contact"}>Hire Us</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
