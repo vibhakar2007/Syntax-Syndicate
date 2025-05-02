8c6dba967998b27d858f6ac693ab4d14371a67b0
Manish Suthar <manishsuthar078@gmail.com>
2025-05-02 13:03:15 +0530

    fix(home): h-extended used and pt-15 to mt-20, moving the content in the center

--- src/app/(screen)/page.tsx
+++ src/app/(screen)/page.tsx
@@ -3,14 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container flex items-center justify-center flex-col gap-4 mx-auto pt-15">
+    <div className="container flex h-extended items-center justify-center flex-col gap-4 mx-auto mt-20">
       <h1 className="lg:text-5xl text-3xl text-center font-bold leading-normal">
         We Build the Website Your
         <br /> Competitors Wish They
         <br /> Had.
       </h1>
       <div className="flex items-center justify-center flex-row gap-4 pt-5">
-        <Button color="gray" size={"lg"} variant={"default"} className="bg-white text-black">
+        <Button
+          color="gray"
+          size={"lg"}
+          variant={"default"}
+          className="bg-white text-black"
+        >
           <Link href={"/work"}>Our Projects</Link>
         </Button>
         <Button variant={"outline"} size={"lg"} asChild>
