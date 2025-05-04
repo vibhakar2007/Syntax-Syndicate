fd6defb263c898da8e165a6d3ce6c63aa0b8cc94
Manish Suthar <manishsuthar078@gmail.com>
2025-05-04 12:01:34 +0530

    misc(eslint)!: eslint config updated
    npm commands updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["dist/", ".next/", "node_modules", "./public/"],
+  },
 ];
 
 export default eslintConfig;
