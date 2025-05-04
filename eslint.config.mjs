e03960267495a03b1664bb3055c2c731ecce3093
Manish Suthar <manishsuthar078@gmail.com>
2025-05-04 12:33:20 +0530

    feat: eslint config prettier added, Prettier commands added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: ["dist/", ".next/", "node_modules", "./public/"],
-  },
-];
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        ignores: ['dist/', '.next/', 'node_modules', './public/'],
+    },
+    eslintConfigPrettier,
+]
 
-export default eslintConfig;
+export default eslintConfig
