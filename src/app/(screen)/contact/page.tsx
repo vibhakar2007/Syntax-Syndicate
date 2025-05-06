e30c916366b656c7163050c8dc19fe8a55bdb73a
Manish Suthar <manishsuthar078@gmail.com>
2025-05-06 20:49:38 +0530

    fix(contact-form): unstable width fixed in contact form textarea

--- src/app/(screen)/contact/page.tsx
+++ src/app/(screen)/contact/page.tsx
@@ -4,7 +4,7 @@ import ContactForm from "@/components/form/ContactForm";
 export default function Contact() {
   return (
     <div className="container grid grid-cols-2 max-md:grid-cols-1 h-extended gap-4 mx-auto mt-20">
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4 w-full">
         <h1 className="lg:text-5xl text-3xl text-center font-bold leading-normal">
           Get in Touch
         </h1>
@@ -13,7 +13,7 @@ export default function Contact() {
           we will get back to you as soon as possible.
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4 w-full">
         <ContactForm />
       </div>
     </div>
--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -61,7 +61,7 @@ const ContactForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
         <FormField
           control={form.control}
           name="name"
@@ -102,7 +102,7 @@ const ContactForm = () => {
                 <Textarea
                   cols={8}
                   rows={10}
-                  className="h-[100px] resize-none md:w-[450px] w-[300px] flex"
+                  className=""
                   placeholder="e.g., We need expertise of SyntaxSyndicate to develop us the best website out there."
                   {...field}
                 />
