7bc6b9de66af2091c17e4896d54b76e53d52840c
m4dd0c <m4dd0c@users.noreply.github.com>
2025-05-16 12:03:14 +0000

    chore: auto-commit prettier fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-	const geistMono = Geist_Mono({
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  			subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
