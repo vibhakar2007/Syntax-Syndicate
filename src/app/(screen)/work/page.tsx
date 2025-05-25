ebbb7e1d88b56f0365666991817d1ba94b3ca549
Manish Suthar <manishsuthar078@gmail.com>
2025-05-25 16:28:45 +0530

    feat: work page added with dummy projects

---
+++ src/app/(screen)/work/page.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { projects } from "@/lib/constants";
+import { WorkCard } from "@/components/card/WorkCard";
+
+function Work() {
+  return (
+    <div className="container min-h-extended mx-auto mt-12 border rounded-3xl p-4">
+      <div className="pl-4">
+        <h1 className="lg:text-4xl text-xl font-semibold leading-normal">
+          Project
+        </h1>
+        <p className="text-muted-foreground text-sm">
+          A collection of my projects in web development, AI, and accessibility
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-center my-4 gap-4 w-full">
+        {projects.map((project, i) => (
+          <WorkCard project={project} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Work;
