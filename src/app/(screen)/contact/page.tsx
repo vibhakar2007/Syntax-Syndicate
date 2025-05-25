8d38b22a733075933c37d176c86d8af02362bae4
Manish Suthar <manishsuthar078@gmail.com>
2025-05-25 15:37:11 +0530

    impr: ui improved

--- src/app/(screen)/contact/page.tsx
+++ src/app/(screen)/contact/page.tsx
@@ -3,12 +3,12 @@ import ContactForm from "@/components/form/ContactForm";
 
 export default function Contact() {
   return (
-    <div className="container grid grid-cols-2 max-md:grid-cols-1 h-extended gap-4 mx-auto mt-20">
+    <div className="container grid grid-cols-2 max-md:grid-cols-1 h-extended gap-10 mx-auto mt-20">
       <div className="flex flex-col items-center justify-center gap-4 w-full">
         <h1 className="lg:text-5xl text-3xl text-center font-bold leading-normal">
           Get in Touch
         </h1>
-        <p className="text-center">
+        <p>
           We are here to help you with your project. Fill out the form below and
           we will get back to you as soon as possible.
         </p>
