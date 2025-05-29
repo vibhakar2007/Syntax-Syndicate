6cd60c8f811c0ce1bf02cc25d9b9b571261268ce
baibhavmandal <baibhavmandal@users.noreply.github.com>
2025-05-29 06:04:11 +0000

    chore: auto-commit prettier fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
@@ -9,13 +9,15 @@ export default function NotFound() {
       <div className="my-2 text-center">
         <h1 className="text-6xl font-bold">404</h1>
         <p className="mt-4 text-xl">Page Not Found</p>
-        <p className="mt-2 text-gray-500">The page you are looking for does not exist.</p>
+        <p className="mt-2 text-gray-500">
+          The page you are looking for does not exist.
+        </p>
       </div>
       <div className="my-2 hidden md:block text-right">
-          <Button variant="default" asChild>
-            <Link href="/">Return Home</Link>
-          </Button>
-        </div>
+        <Button variant="default" asChild>
+          <Link href="/">Return Home</Link>
+        </Button>
+      </div>
     </div>
   );
 }
